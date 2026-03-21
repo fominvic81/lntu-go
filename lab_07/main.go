0cb188c692a6760022a2acc2d3ec6408acc37bec
fominvic81 <fomin.vic81@gmail.com>
2026-03-21 14:29:31 +0200

    lab 7

--- lab_07/main.go
+++ lab_07/main.go
@@ -1,21 +1,23 @@
 ﻿package main
 
 import (
+	"github.com/fominvic81/lntu-go/lab_07/notes"
 	"github.com/gofiber/fiber/v3"
 	"github.com/mailru/easyjson"
+	"maps"
+	"net/http"
 	"slices"
 	"strconv"
-
-	"github.com/fominvic81/lntu-go/lab_07/notes"
 )
 
 func main() {
 	app := fiber.New()
 
-	notesArr := notes.Notes{}
+	notesMap := map[string]notes.Note{}
+	lastId := 1
 
 	app.Get("/notes", func(c fiber.Ctx) error {
-		response, err := easyjson.Marshal(notesArr)
+		response, err := easyjson.Marshal(notes.Notes(slices.Collect(maps.Values(notesMap))))
 		if err != nil {
 			return err
 		}
@@ -28,19 +30,18 @@ func main() {
 
 	app.Get("/notes/:id", func(c fiber.Ctx) error {
 		id := c.Params("id")
-		for _, note := range notesArr {
-			if strconv.FormatInt(int64(note.Id), 10) == id {
-				response, err := easyjson.Marshal(note)
-				if err != nil {
-					return err
-				}
-				if err := c.SendString(string(response)); err != nil {
-					return err
-				}
-				return nil
+		note, ok := notesMap[id]
+		if !ok {
+			if err := c.Status(http.StatusNotFound).SendString("note not found"); err != nil {
+				return err
 			}
+			return nil
 		}
-		if err := c.Status(404).SendString("note not found"); err != nil {
+		response, err := easyjson.Marshal(note)
+		if err != nil {
+			return err
+		}
+		if err := c.SendString(string(response)); err != nil {
 			return err
 		}
 		return nil
@@ -51,7 +52,10 @@ func main() {
 		if err := easyjson.Unmarshal(c.Body(), &note); err != nil {
 			return err
 		}
-		notesArr = append(notesArr, note)
+		note.Id = lastId
+		lastId += 1
+
+		notesMap[strconv.Itoa(note.Id)] = note
 
 		response, err := easyjson.Marshal(note)
 		if err != nil {
@@ -66,40 +70,43 @@ func main() {
 
 	app.Put("/notes/:id", func(c fiber.Ctx) error {
 		id := c.Params("id")
-		for i, note := range notesArr {
-			if strconv.FormatInt(int64(note.Id), 10) == id {
-				newNote := notes.Note{}
-				if err := easyjson.Unmarshal(c.Body(), &newNote); err != nil {
-					return err
-				}
-				if newNote.Id != note.Id {
-					if err := c.Status(400).SendString("Can not modify note id"); err != nil {
-						return err
-					}
-				}
-				notesArr[i] = newNote
-				response, err := easyjson.Marshal(newNote)
-				if err != nil {
-					return err
-				}
-				if err := c.SendString(string(response)); err != nil {
-					return err
-				}
-				return nil
+		note, ok := notesMap[id]
+		if !ok {
+			if err := c.Status(http.StatusNotFound).SendString("note not found"); err != nil {
+				return err
 			}
+			return nil
 		}
-		if err := c.Status(404).SendString("note not found"); err != nil {
+		newNote := notes.Note{}
+		if err := easyjson.Unmarshal(c.Body(), &newNote); err != nil {
 			return err
 		}
+		newNote.Id = note.Id
 
+		notesMap[id] = newNote
+		response, err := easyjson.Marshal(newNote)
+		if err != nil {
+			return err
+		}
+		if err := c.SendString(string(response)); err != nil {
+			return err
+		}
 		return nil
 	})
 
 	app.Delete("/notes/:id", func(c fiber.Ctx) error {
 		id := c.Params("id")
-		notesArr = slices.DeleteFunc(notesArr, func(note *notes.Note) bool {
-			return strconv.FormatInt(int64(note.Id), 10) == id
-		})
+		_, ok := notesMap[id]
+		if !ok {
+			if err := c.Status(http.StatusNotFound).SendString("note not found"); err != nil {
+				return err
+			}
+			return nil
+		}
+		delete(notesMap, id)
+		if err := c.SendStatus(http.StatusNoContent); err != nil {
+			return err
+		}
 		return nil
 	})
 
